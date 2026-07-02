0e25b5f74c54abab0dbac9718f1663a656c45252
JASKSG9 <jasksg9@gmail.com>
2026-07-01 20:39:26 -0400

    Delete AR5IFACTS directory

--- AR5IFACTS/PSEUDOSPECTRAL_d.py
+++
@@ -1,8 +0,0 @@
-# Skeleton for generating the epsilon-pseudospectrum of D
-import numpy as np
-from scipy.linalg import svd, norm
-
-def pseudospectrum_contour(D, epsilon=1e-2):
-    # Compute the resolvent (zI - D)^-1 for a grid of complex z
-    # The norm of the resolvent tells you exactly how robust your defect operator is.
-    pass 
